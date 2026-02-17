576590d4c8e152f681507be9e7a9ffdcef5e6021
bruwbird <stm1051212@yahoo.co.jp>
2026-02-18 00:26:04 +0900

    Set up direnv and update flake inputs (#6526)
    Signed-off-by: Yusuke Shimizu <stm1051212@gmail.com>

--- flake.nix
+++ flake.nix
@@ -30,9 +30,7 @@
         
         darwinInputs = with pkgs; [
           libiconv
-          darwin.apple_sdk.frameworks.Security
-          darwin.apple_sdk.frameworks.SystemConfiguration
-          darwin.apple_sdk.frameworks.CoreServices
+          apple-sdk
         ];
         
         buildInputs = commonInputs
